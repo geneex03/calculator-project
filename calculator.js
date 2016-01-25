40dfa0d9edc8f190d1e075d5a1d193e733667cec
geneex03 <genesis.paulino@mihsnyc.org>
2016-01-25 14:32:30 +0000

    add function to my calculator so it can resolve the math problems

--- calculator.js
+++ calculator.js
@@ -133,4 +133,30 @@ function divide(x,y)
 {
   return x/y;
 }
+
+function multiply(x,y){
+	return x*y;
+}
+
+function add(x,y){
+	return x+y;
+}
+function subtract(x,y){
+	return x-y;
+}
+
+function power(x,y){
+	return x^y;
+}
+
+function square_root(x){
+	return Math.sqrt(x);
+}
+function logBase10(x){
+	return Math.log(x);
+}
+
+function generateRandomNumber () {
+	return Math.random();
+}
 // You must implement the rest of the functions here.
